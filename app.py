80b92d8c61d2f464c3357b26d8259632f21274ac
twoaun <55779613+twoaun@users.noreply.github.com>
2019-09-25 13:52:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,11 @@ def callback():
     json_line = request.get_json()
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
-    user = decoded["events"][0]['replyToken']
-    userText = decoded["events"][0]['message']['text']
-    #sendText(user,userText)
+    user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
+    #user = decoded["events"][0]['replyToken']
+    userText = decoded['queryResult']['intent']['displayName']
+    #userText = decoded["events"][0]['message']['text']
+    sendText(user,userText)
     if (userText == 'สวัสดี') :
         sendText(user,'เออดีด้วย')
     elif (userText == 'ไอ้บ้า') :
